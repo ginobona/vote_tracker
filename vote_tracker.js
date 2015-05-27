2b4f27c432f7998d316093f9845275d8d512b98c
GINO BONA <GINO@GINOBONA.COM>
2015-05-27 09:31:59 -0700

    Initial pass at jQuery assignment

---
+++ vote_tracker.js
@@ -0,0 +1,44 @@
+<script type="text/javascript">
+
+function Kitten(image, wins, losses) {
+  this.image = image;
+  this.wins = wins;
+  this.losses = losses;
+} /* Need to calculate losses so the photo that isn't selected does not appear again */
+
+var kittenPics = [];
+  kittenPics.push('images/01.jpg'));
+  kittenPics.push('images/02.jpg'));
+  kittenPics.push('images/03.jpg'));
+  kittenPics.push('images/04.jpg'));
+  kittenPics.push('images/05.jpg'));
+  kittenPics.push('images/06.jpg'));
+  kittenPics.push('images/07.jpg'));
+  kittenPics.push('images/08.jpg'));
+  kittenPics.push('images/09.jpg'));
+  kittenPics.push('images/10.jpg'));
+  kittenPics.push('images/11.jpg'));
+  kittenPics.push('images/12.jpg'));
+  kittenPics.push('images/13.jpg'));
+  kittenPics.push('images/14.jpg'));
+
+var showKitten = function () {
+
+}
+
+/* FIRST FUNCTION */
+//Create a function that will randomly select an image between 01.jpg and 14.jpg and place image in section id = FirstPhoto; use a prepend element?//
+
+/* SECOND FUNCTION */
+//Create a function that will randomly select an image between 01.jpg and 14.jpg but not the same one as First Function; place image in section id = SecondPhoto; use a prepend element?//
+
+/* VOTE */
+/* Create event that highlights photo user voted for (i.e., animation wiggle, glow, etc.)
+
+
+/* PROMPT TO VOTE AGAIN */
+/* Prompt the user to review and vote on two new cat photos */
+
+/* REPEAT FIRST AND SECOND FUNCTIONS */
+
+
